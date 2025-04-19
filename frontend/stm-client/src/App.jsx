6f8e05625bcc0602f838baf468b8692680abe456
PramodChamalka <shanuthpchamalka01@gmail.com>
2025-04-19 22:10:05 +0530

    done

--- frontend/stm-client/src/App.jsx
+++ frontend/stm-client/src/App.jsx
@@ -4,27 +4,42 @@ import Home from './Home'
 import StudentsView from './components/student/StudentsView'
 import { BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import NavBar from './components/comman/NavBar'
+import AddStudent from './components/student/AddStudent'
+import EditStudent from './components/student/EditStudent'
+import StudentProfile from './components/student/StudentProfile'  
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-
-      <h2>Hello</h2>
-      {/* <Home />
-      <StudentsView /> */}
-      
+    <main className='container mt-5'>
       <Router>
-      <NavBar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/view-students" element={<StudentsView />} />
-        </Routes>
-      </Router>
-        
-    </>
-  )
+				<NavBar />
+				<Routes>
+					<Route
+						exact
+						path="/"
+						element={<Home />}></Route>
+					<Route
+						exact
+						path="/view-students"
+						element={<StudentsView />}></Route>
+					<Route
+						exact
+						path="/add-students"
+						element={<AddStudent />}></Route>
+					<Route
+						exact
+						path="/edit-student/:id"
+						element={<EditStudent />}></Route>
+					<Route
+						exact
+						path="/student-profile/:id"
+						element={<StudentProfile />}></Route>
+				</Routes>
+			</Router>
+		</main>
+	);
 }
 
 export default App
--- frontend/stm-client/src/components/comman/NavBar.jsx
+++ frontend/stm-client/src/components/comman/NavBar.jsx
@@ -3,24 +3,45 @@ import { Link } from 'react-router-dom'
 
 
 const NavBar = () => {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <Link className="navbar-brand" to={"/"}>Navbar</Link>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <div className="collapse navbar-collapse" id="navbarNav">
-    <ul className="navbar-nav">
-      <li className="nav-item active">
-        <Link className="nav-Link" to={"/view-students"}> View All Students <span className="sr-only"></span></Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-Link" to={"/Add-Students"}>Add Student</Link>
-      </li>
-    </ul>
-  </div>
-</nav>
-  )
+  return (<nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-5">
+    <div className="container-fluid">
+      <Link className="navbar-brand" to={"/"}>
+        SBR Demo
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div
+        className="collapse navbar-collapse"
+        id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link
+              className="nav-link active"
+              aria-current="page"
+              to={"/view-students"}>
+              View All Student
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              className="nav-link"
+              to={"/add-students"}>
+              Add new Students
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+);
 }
 
 export default NavBar
